26eae1dec1956e137214449c0f351e47fcbd64be
solved-class <solved.class@gmail.com>
2026-03-19 23:28:08 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	// 변수 선언
+	int a, b; 
+	
+	// 입력
+	cin >> a >> b; 
+	
+	// 덧셈 연산
+	a = a + b; 
+	
+	// 출력
+	cout << a; 
+	
+	return 0;
+}
